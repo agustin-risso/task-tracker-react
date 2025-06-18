c3818b510f1fcc91bbd529e79202ceb8182fb466
Agustin Risso <agustin.risso@renaiss.io>
2025-06-18 10:51:32 -0300

    add delete button functionality to each task

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -15,6 +15,11 @@ function Todo() {
 		setNewTask("");
 	}
 
+	function deleteTask(id) {
+		const filteredList = todoList.filter((task) => task.id !== id);
+		setTodoList(filteredList);
+	}
+
 	return (
 		<div
 			style={{
@@ -42,15 +47,40 @@ function Todo() {
 				style={{
 					textAlign: "left",
 					width: "100%",
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "start",
+					gap: "4px",
 					height: "300px",
 					overflowY: "auto",
 					padding: "0",
 				}}
 			>
 				{todoList.map((task, key) => (
-					<li style={{ listStyle: "none" }} key={key}>
+					<li
+						style={{
+							listStyle: "none",
+							display: "flex",
+							width: "100%",
+							alignItems: "center",
+							width: "100%",
+						}}
+						key={key}
+					>
 						<input type="checkbox"></input>
 						{task.taskName}
+						<button
+							style={{
+								color: "white",
+								backgroundColor: "red",
+								padding: "3px 6px",
+								marginLeft: "auto",
+								fontSize: ".75rem",
+							}}
+							onClick={() => deleteTask(task.id)}
+						>
+							delete
+						</button>
 					</li>
 				))}
 			</ul>
